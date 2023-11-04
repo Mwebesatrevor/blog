797bca98f33425bb027e9343dd367a422c4bac27
trevormwebesa <trevormwebesa@gmail.com>
2023-11-04 16:48:04 +0300

    some other stuff

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import Create from './Create';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   const title = 'App Component';
@@ -16,6 +17,7 @@ function App() {
             <Route exact path="/" element={<Home />} />
             <Route exact path="/create" element={<Create />} />
             <Route exact path="/blogs/:id" element={<BlogDetails />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
       </div>
--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,58 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario');
+    const [isPending, setIsPending] = useState(false);
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); // prevent page refresh
+        const blog = { title, body, author };
+        setIsPending(true);
+
+        fetch('http://localhost:4000/blogs', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log('new blog added');
+            setIsPending(false);
+            // history.go(-1); // go back to previous page
+            navigate('/'); // go to home page
+        })
+    }
+
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog title:</label>
+                <input 
+                type="text"
+                required
+                value={title}
+                onChange={(e) => setTitle(e.target.value)} 
+                />
+                <label>Blog body:</label>
+                <textarea
+                required
+                value={body}
+                onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+                <label>Blog author:</label>
+                <select
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                { !isPending && <button>Add Blog</button>}
+                { isPending && <button>Adding Blog...</button>}
+            </form>
         </div>
      );
 }
